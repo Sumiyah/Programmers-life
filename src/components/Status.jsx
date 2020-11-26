18d34b7a4507aaf6609191232f63cdb7c6bf5207
Sumiyah <sumiyah.fa@gmail.com>
2020-11-26 15:57:21 +0300

    changes in status and views

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,48 +1,33 @@
 import React, { useState } from 'react'
 
 function Status ({status}) {
-
-
   return (
     <>
-    <div className="row">
-      <div className="col">
-        <label>Energy:</label>
-        <div className="progress">
-          <div className="progress-bar progress-bar-striped bg-info progress-bar-animated " role="progressbar" style={{width: `${status.energy}%` } } ></div>
+      <div className="row justify-content-center border py-3">
+        <div className="col-3">
+          <label>Energy:</label>
+          <div className="progress">
+            <div className="progress-bar progress-bar-striped bg-info progress-bar-animated " role="progressbar" style={{width: `${status.energy}%` } } ></div>
+          </div>
+        </div>
+        <div className="col-3">
+          <label>Fullness:</label>
+          <div className="progress">
+            <div className="progress-bar progress-bar-striped bg-danger progress-bar-animated " role="progressbar" style={{width: `${status.fullness}%` } } ></div>
+          </div>
+        </div>
+        <div className="col-3">
+          <label>Happiness:</label>
+          <div className="progress">
+            <div className="progress-bar progress-bar-striped bg-success progress-bar-animated " role="progressbar" style={{width: `${status.happiness}%` } } ></div>
+          </div>
         </div>
       </div>
-      <div className="col">
-        <label>Fullness:</label>
-        <div className="progress">
-          <div className="progress-bar progress-bar-striped bg-danger progress-bar-animated " role="progressbar" style={{width: `${status.fullness}%` } } ></div>
-        </div>
+
+      <div className="row justify-content-center border py-2">
+          <div className="col-2">Money: <i class="far fa-money-bill-alt"></i> x{status.money}</div>
+          <div className="col-2">Meals: <i class="fas fa-hamburger"></i> x{status.meals}</div>
       </div>
-      <div className="col">
-        <label>Happiness:</label>
-        <div className="progress">
-          <div className="progress-bar progress-bar-striped bg-success progress-bar-animated " role="progressbar" style={{width: `${status.happiness}%` } } ></div>
-        </div>
-      </div>
-      
-      
-    </div>
-
-
-
-
-    <div className="row justify-content-center">
-    {/* money and meals */}
-    <div className="col-3">
-      <label>Money:</label>
-      <p><i class="far fa-money-bill-alt"></i> x{status.money}</p>
-    </div>
-    <div className="col-3">
-      <label>Meals:</label>
-      <p><i class="fas fa-hamburger"></i> x{status.meals}</p>
-    </div>
-    </div>
-
     </>
   );
 }

b7b2aa1ba3006a0e47baad24bd633ea37ad88fa3
wijdan1995 <wijdan.w.k@outlook.sa>
2020-11-29 14:58:28 +0300

    add build folder

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function View({status}) {
     return (
         <div className="col text-center">
-            <img style={{height: "300px", width: "250px"}} src={status.image} alt="img" />
+            <img style={{height: "300px", width: "250px"}} src={`/${status.image}`} alt="img" />
             <h4 className="mt-3"> {status.message}</h4>
         </div>
     );

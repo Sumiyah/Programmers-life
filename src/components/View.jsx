03e6e9a8c1340f90c71a65f492cc1572dc755335
YaraAljarbou <yara.aljarbou@yahoo.com>
2020-12-12 17:45:05 +0300

    changing view

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 
 function View({status}) {
     return (
+        <>
         <div className="col text-center">
             <img style={{height: "300px", width: "250px"}} src={status.image} alt="img" />
-            <h5 className="mt-3 border rounded" style={{backgroundColor: "#E5E5E5", 'white-space': 'pre-wrap'}}> {status.message}</h5>
         </div>
+        <div className="col text-center">
+        <h5 className="mt-3 border rounded text-center p-2" style={{backgroundColor: "#E5E5E5", 'white-space': 'pre-wrap', display: 'inline-block'}}> {status.message}</h5>
+        </div>
+        
+        </>
     );
 }
 
